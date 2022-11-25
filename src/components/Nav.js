a9b3d873ab14c083f9b65fcf23431b8b23b8fc46
to06109 <69625013+to06109@users.noreply.github.com>
2022-11-25 16:50:07 +0900

    feat(All): 메인화면 데이터 스프레드 구현

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav id="nav">
+      <div className="nav">
+        <div id="icon">
+          <img src="assets/img/menu.png" />
+        </div>
+        <div>
+          <a href="#">서랍</a>
+        </div>
+        <div id="icon">
+          <img src="assets/img/user.png" />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
