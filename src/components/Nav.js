d6c91e1cfa382cff42b19a3c4b73088c2fae1205
to06109 <69625013+to06109@users.noreply.github.com>
2022-11-25 17:19:19 +0900

    feat(Nav): 카테고리 사이드 바 템플릿 적용

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,39 @@
 import React from 'react'
 
 function Nav() {
+  const openNav = () => {
+    document.getElementById('mySidenav').style.width = '250px'
+  }
+
+  const closeNav = () => {
+    document.getElementById('mySidenav').style.width = '0'
+  }
   return (
-    <nav id="nav">
-      <div className="nav">
-        <div id="icon">
-          <img src="assets/img/menu.png" />
-        </div>
-        <div>
-          <a href="#">서랍</a>
-        </div>
-        <div id="icon">
-          <img src="assets/img/user.png" />
-        </div>
+    <div>
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>
+          &times;
+        </a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
       </div>
-    </nav>
+      <nav id="nav">
+        <div className="nav">
+          <div id="icon" onClick={openNav}>
+            <img src="assets/img/menu.png" />
+          </div>
+
+          <div>
+            <a href="#">서랍</a>
+          </div>
+          <div id="icon">
+            <img src="assets/img/user.png" />
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
