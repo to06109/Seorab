a41c5f67e8e813b120b67682ac9ef9b0b657f635
Yesong <to06109@naver.com>
2022-11-27 03:49:29 +0900

    feat(Login): 로그인 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const catagoryList = [
   'Recent',
@@ -6,63 +6,63 @@ const catagoryList = [
   'fashion',
   'drawing',
   'assignment',
-]
+];
 
 function Nav() {
   // 카테고리 목록 가져오기
   useEffect(() => {
     catagoryList.forEach((category) => {
-      const a = document.createElement('a')
-      a.href = '#'
-      a.innerText = category
-      document.getElementById('categoryList').appendChild(a)
-    })
-  })
+      const a = document.createElement('a');
+      a.href = '#';
+      a.innerText = category;
+      document.getElementById('categoryList').appendChild(a);
+    });
+  });
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const openCategory = () => {
-    document.getElementById('mySidenav').style.width = '300px'
-  }
+    document.getElementById('mySidenav').style.width = '300px';
+  };
 
   const closeCategory = () => {
-    document.getElementById('mySidenav').style.width = '0'
-  }
+    document.getElementById('mySidenav').style.width = '0';
+  };
 
   const openLogin = () => {
-    window.location.href = '/login'
-  }
+    window.location.href = '/login';
+  };
 
   const openRegister = () => {
-    window.location.href = '/register'
-  }
+    window.location.href = '/register';
+  };
   return (
     <div>
-      <div id="mySidenav" class="sidenav">
-        <div id="categoryList">
-          <a href="javascript:void(0)" class="closebtn" onClick={closeCategory}>
+      <div id='mySidenav' class='sidenav'>
+        <div id='categoryList'>
+          <a href='javascript:void(0)' class='closebtn' onClick={closeCategory}>
             &times;
           </a>
-          <div id="cate_title">Catagory</div>
+          <div id='cate_title'>Catagory</div>
         </div>
-        <div class="add_btn">Add Catagory</div>
+        <div class='add_btn'>Add Catagory</div>
       </div>
-      <nav id="nav">
-        <div className="nav">
-          <div id="icon" onClick={openCategory}>
-            <img src="assets/img/menu.png" />
+      <nav id='nav'>
+        <div className='nav'>
+          <div id='icon' onClick={openCategory}>
+            <img src='assets/img/menu.png' />
           </div>
 
-          <div className="logo">
-            <a href="/">서랍</a>
+          <div className='logo'>
+            <a href='/'>서랍</a>
           </div>
-          <div className="empty" />
+          <div className='empty' />
           {/* ------------마이페이지------------- */}
-          <div id="icon" onClick={() => setIsOpen(!isOpen)}>
-            <img src="assets/img/user.png" />
+          <div id='icon' onClick={() => setIsOpen(!isOpen)}>
+            <img src='assets/img/user.png' />
           </div>
           {isOpen && (
-            <div className="select">
+            <div className='select'>
               <ul>
                 <div onClick={openLogin}>로그인</div>
                 <div onClick={openRegister}>회원가입</div>
@@ -72,7 +72,7 @@ function Nav() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,12 +1,48 @@
-import Nav from '../components/Nav'
+import { useState } from 'react';
+import Nav from '../components/Nav';
 
 export default function Login() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const LoginSubmit = (e) => {
+    e.preventDefault();
+    setId(e.target.id.value);
+    setPassword(e.target.password.value);
+
+    // 서버에 보낼 데이터
+    // 처음 console에 찍을 때 반영 안되는 오류 있음
+    let data = {
+      id: id,
+      password: password,
+    };
+    console.log(data);
+  };
+
   return (
-    <div id="login">
+    <div id='login'>
       <Nav />
-      <div id="container">
-        <div className="box"></div>
+      <div id='container'>
+        <div className='box'>
+          <form className='flex_col top_margin' onSubmit={LoginSubmit}>
+            <input type='text' required name='id' placeholder=' 아이디'></input>
+            <input
+              type='password'
+              required
+              name='password'
+              placeholder=' 비밀번호'
+            ></input>
+            <button>로그인</button>
+          </form>
+          <div className='flex_row center'>
+            <a href='/'>아이디찾기 | </a>
+            <a href='/'>비밀번호찾기 | </a>
+            <a href='/register'>회원가입</a>
+          </div>
+
+          {/* 소셜로그인 */}
+        </div>
       </div>
     </div>
-  )
+  );
 }
