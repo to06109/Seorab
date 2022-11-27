2ecdc88809c34dca213cd17e60662b443785c656
to06109 <69625013+to06109@users.noreply.github.com>
2022-11-28 02:13:04 +0900

    feat(Login): 로그인 백엔드 연동 코드 구현

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,48 +1,79 @@
-import { useState } from 'react';
-import Nav from '../components/Nav';
+import { useState } from 'react'
+import Nav from '../components/Nav'
 
 export default function Login() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  const [data, setData] = useState({
+    id: '',
+    password: '',
+  })
 
-  const LoginSubmit = (e) => {
-    e.preventDefault();
-    setId(e.target.id.value);
-    setPassword(e.target.password.value);
+  const { id, password } = data
 
-    // 서버에 보낼 데이터
-    // 처음 console에 찍을 때 반영 안되는 오류 있음
-    let data = {
-      id: id,
-      password: password,
-    };
-    console.log(data);
-  };
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setData({
+      ...data,
+      [name]: value,
+    })
+  }
+
+  const reqData = JSON.stringify(data)
+
+  // 백엔드 연동할 때 풀기
+  // const [resData, setResdata] = useState('');
+  // const url = 'http://127.0.0.1:8000/test/getMembers/' ;
+
+  const onClick = async (e) => {
+    e.preventDefault()
+    console.log(id, password)
+    console.log(reqData)
+
+    // 백엔드 연동할 때 풀기
+    // try{
+    //   const response = await axios.post(url, reqData,{
+    //     headers: {
+    //       // Overwrite Axios's automatically set Content-Type
+    //       'Content-Type': 'application/json'
+    //     }
+    //   });
+    //   setResdata(response.data);
+    // } catch (e) {
+    //   console.log(e)
+    //   alert("해당 id가 존재하지 않습니다.")
+    // }
+  }
 
   return (
-    <div id='login'>
+    <div id="login">
       <Nav />
-      <div id='container'>
-        <div className='box'>
-          <form className='flex_col top_margin' onSubmit={LoginSubmit}>
-            <input type='text' required name='id' placeholder=' 아이디'></input>
+      <div id="container">
+        <div className="box">
+          <form className="flex_col top_margin" onSubmit={onClick}>
             <input
-              type='password'
+              name="id"
+              type="text"
               required
-              name='password'
-              placeholder=' 비밀번호'
+              placeholder=" 아이디"
+              onChange={onChange}
             ></input>
-            <button>로그인</button>
+            <input
+              type="password"
+              required
+              placeholder=" 비밀번호"
+              name="password"
+              onChange={onChange}
+            ></input>
+            <button type="submit">로그인</button>
           </form>
-          <div className='flex_row center'>
-            <a href='/'>아이디찾기 | </a>
-            <a href='/'>비밀번호찾기 | </a>
-            <a href='/register'>회원가입</a>
+          <div className="flex_row center">
+            <a href="/">아이디찾기 | </a>
+            <a href="/">비밀번호찾기 | </a>
+            <a href="/register">회원가입</a>
           </div>
 
           {/* 소셜로그인 */}
         </div>
       </div>
     </div>
-  );
+  )
 }
